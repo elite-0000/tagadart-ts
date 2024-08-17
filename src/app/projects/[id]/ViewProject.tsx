c165a2bb0e06d8f5701123938952d9a283672f40
elite-0000 <elite.alphapoint1990@gmail.com>
2024-08-17 15:51:34 +0200

    add projects folder for example

---
+++ src/app/projects/[id]/ViewProject.tsx
@@ -0,0 +1,28 @@
+'use client'
+import React from 'react'
+
+import useSWR from 'swr'
+
+import { fetchAxiosAPI } from '@/request/request'
+import { BaseData, FormValues } from '@/types/global'
+
+type Props = {
+  params: any
+}
+
+export const ViewProject = ({ params: { id } }: Props) => {
+  const { data: projectData } = useSWR<BaseData>(
+    `/projects/${id}`,
+    (url: string) => fetchAxiosAPI(url),
+  )
+  if (!projectData) return null
+
+  const project = projectData.data
+
+  return (
+    <div>
+      <h1>Test</h1>
+      <h1>{project.client}</h1>
+    </div>
+  )
+}
