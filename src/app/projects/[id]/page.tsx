d12254bd0e5289207c46dc230e122bf8940a7dc9
elite-0000 <elite.alphapoint1990@gmail.com>
2024-08-18 11:17:09 +0200

    fix: test

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from 'next'
 import { ViewProject } from './ViewProject'
 
-type Props = {
-  id: string
-  initialData: any
-  params: any
-}
-
 export const metadata: Metadata = {
   title: 'Projet - Nom du projet',
 }
 
-export default function ViewProjectPage({ params }: Props) {
-  console.log(params, 'params')
+export default function ViewProjectPage({ params }: { params: any }) {
   return <ViewProject params={params} />
 }
