4c4a9e8cd5452499d1da57bda31aee7e28370fb3
elite-0000 <elite.alphapoint1990@gmail.com>
2024-08-18 11:21:47 +0200

    fix: build

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from 'next'
 import { ViewProjects } from './ViewProjects'
 
-type Props = {
-  id: string
-  initialData: any
-  params: any
-}
-
 export const metadata: Metadata = {
   title: 'Projets',
 }
 
-export default function ViewProjectsPage({ params }: Props) {
+export default function ViewProjectsPage() {
   return <ViewProjects />
 }
--- src/app/work/1/page.tsx
+++ src/app/work/1/page.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from 'next'
 import { ViewProject } from './ViewProject'
 
-type Props = {
-  id: string
-  initialData: any
-  params: any
-}
-
 export const metadata: Metadata = {
   title: 'Projet - Nom du projet',
 }
 
-export default function EditActualityPage({ params }: Props) {
-  console.log(params, 'params')
+export default function EditActualityPage({ params }: { params: any }) {
   return <ViewProject params={params} />
 }
