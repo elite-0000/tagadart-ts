bcad204c61dc98f23b6b0d0773d962209cbeb523
elite-0000 <elite.alphapoint1990@gmail.com>
2024-08-26 10:45:17 -0400

    remove unncessary code

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 }
 
 export default async function Blog() {
-  let articles = await loadArticles()
+  
   const populateBlog = [
     'pageIntro',
     'blogSection',
