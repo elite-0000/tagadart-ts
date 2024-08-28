5a822c2317b00004906252bfe899e9b4730ee5b1
elite-0000 <elite.alphapoint1990@gmail.com>
2024-08-28 15:56:46 -0400

    update project list in ViewProject

---
+++ src/app/work/ViewWorks.tsx
@@ -0,0 +1,70 @@
+import { type Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Border } from '@/components/Border'
+import { Button } from '@/components/Button'
+import { ContactSection } from '@/components/sections/ContactSection'
+import { Container } from '@/components/Container'
+import { FadeIn } from '@/components/FadeIn'
+import { PageIntro } from '@/components/sections/PageIntro'
+import { formatDate } from '@/lib/formatDate'
+import { loadArticles } from '@/lib/mdx'
+import { fetchAxiosAPI } from '@/request/request'
+import { RestQueryParams } from '@/types/global'
+import ArticleCard from '@/components/ArticleCard' // Added import statement
+import WorkCard from '@/components/WorkCard'
+
+export default async function ViewWorks() {
+  
+  const populateWork = [
+    'pageIntro',
+    'projectsSection',
+    'projectsSection.projects',
+  ]
+
+  const defaultQueryParams: RestQueryParams = {
+    populate: populateWork,
+    publicationState: 'preview',
+    pagination: {
+      page: 1,
+      pageSize: 10,
+    },
+  }
+
+  let projectData;
+  try {
+    projectData = await fetchAxiosAPI('projects-page', defaultQueryParams)
+  } catch (error) {
+    // Handle the error appropriately here
+    console.error('Failed to load projects data:', error)
+    return <div>Failed to load data</div>
+  }
+  const { pageIntro, projectsSection } = projectData?.data
+  const {projects} = projectsSection;
+  console.log("projects:", projects);
+
+  return (
+    <>
+      <PageIntro {...pageIntro}>
+        <p>{pageIntro.content}</p>
+      </PageIntro>
+
+      <Container className="mt-24 sm:mt-32 lg:mt-40">
+        <FadeIn>
+          <h2 className="font-display text-2xl font-semibold text-neutral-950">
+            Nos projets
+          </h2>
+        </FadeIn>
+        <div className="space-y-24 lg:space-y-32">
+          {projects && projects.map((project: any) => (
+              // <ArticleCard article={post} key={post.id}/>
+              <WorkCard key={project.id} project={project} />
+          ))}
+        </div>
+      </Container>
+
+      <ContactSection />
+    </>
+  )
+}
---
+++ src/components/WorkCard.tsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import { FadeIn } from '@/components/FadeIn'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Blockquote } from '@/components/Blockquote'
+import { Border } from '@/components/Border'
+
+import { Button } from '@/components/Button'
+import { PageIntro } from '@/types/global'
+
+interface WorkProps {
+  id: number;
+  client: string
+  content: string
+  logo: string
+  service: string
+  date: string
+  href: string
+  title: string
+  summary: string[]
+  PageIntro: PageIntro
+  testimonial?: {
+    author: string
+    content: string
+    avatar: any
+  }
+}
+
+interface WorkCardProps {
+  project: WorkProps
+}
+
+const WorkCard: React.FC<WorkCardProps> = ({ project }) => {
+  console.log('project', project);
+  const sentencesArray = project.content.split('\n').filter(line => line.trim() !== '');
+
+
+  return (
+    
+    <FadeIn key={project.client}>
+        <div className="mt-10 space-y-20 sm:space-y-24 lg:space-y-32">
+            <article>
+                <Border className="grid grid-cols-3 gap-x-8 gap-y-8 pt-16">
+                <div className="col-span-full sm:flex sm:items-center sm:justify-between sm:gap-x-8 lg:col-span-1 lg:block">
+                    <div className="sm:flex sm:items-center sm:gap-x-6 lg:block">
+                    <Image
+                        src={project.logo}
+                        alt=""
+                        className="h-16 w-16 flex-none"
+                        unoptimized
+                    />
+                    <h3 className="mt-6 text-sm font-semibold text-neutral-950 sm:mt-0 lg:mt-8">
+                        {project.client}
+                    </h3>
+                    </div>
+                    <div className="mt-1 flex gap-x-4 sm:mt-0 lg:block">
+                    <p className="text-sm tracking-tight text-neutral-950 after:ml-4 after:font-semibold after:text-neutral-300 after:content-['/'] lg:mt-2 lg:after:hidden">
+                        {project.service}
+                    </p>
+                    <p className="text-sm text-neutral-950 lg:mt-2">
+                        <time dateTime={project.date}>
+                        {/* {formatDate(caseStudy.date)} */}
+                        {project.date}
+                        </time>
+                    </p>
+                    </div>
+                </div>
+                <div className="col-span-full lg:col-span-2 lg:max-w-2xl">
+                    <p className="font-display text-4xl font-medium text-neutral-950">
+                    <Link href={`/project/${project.id}`}>{project.title}</Link>
+                    </p>
+                    <div className="mt-6 space-y-6 text-base text-neutral-600">
+                        {sentencesArray.map((paragraph) => (
+                            <p key={paragraph}>{paragraph}</p>
+                        ))}
+                    </div>
+                    <div className="mt-8 flex">
+                    <Button
+                        href={`/project/${project.id}`}
+                        aria-label={`Read case study: ${project.client}`}
+                    >
+                        Read case study
+                    </Button>
+                    </div>
+                    {project.testimonial && (
+                    <Blockquote
+                        author={project.testimonial.avatar}
+                        className="mt-12"
+                    >
+                        {project.testimonial.content}
+                    </Blockquote>
+                    )}
+                </div>
+                </Border>
+            </article>
+        </div>
+    </FadeIn>
+  )
+}
+
+export default WorkCard
