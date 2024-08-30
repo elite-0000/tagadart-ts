26b1371b04eebd8c23cc0400817525f796f048d8
elite-0000 <elite.alphapoint1990@gmail.com>
2024-08-30 11:51:26 -0400

    creating component to manage mdx

---
+++ src/components/message-markdown.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from "react"
+import remarkGfm from "remark-gfm"
+import remarkMath from "remark-math"
+import { MessageMarkdownMemoized } from "./message-markdown-memoized"
+
+interface MessageMarkdownProps {
+  content: string
+}
+
+export const MessageMarkdown: FC<MessageMarkdownProps> = ({ content }) => {
+  return (
+    <MessageMarkdownMemoized
+      className="prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 min-w-full space-y-6 break-words"
+      remarkPlugins={[remarkGfm, remarkMath]}
+      components={{
+        p({ children }) {
+          return <p className="mb-2 last:mb-0">{children}</p>
+        },
+        img({ node, ...props }) {
+          return <img className="max-w-[67%]" {...props} />
+        },
+      }}
+    >
+      {content}
+    </MessageMarkdownMemoized>
+  )
+}
