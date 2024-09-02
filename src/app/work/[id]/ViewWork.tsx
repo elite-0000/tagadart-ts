efecf3e28181ea2d3676a3dcad127a03c6f22271
elite-0000 <elite.alphapoint1990@gmail.com>
2024-09-02 05:37:19 -0400

    code review all pages

--- src/app/work/[id]/ViewWork.tsx
+++ src/app/work/[id]/ViewWork.tsx
@@ -1,47 +1,25 @@
 'use client'
-import React from 'react'
-import { FadeIn } from '@/components/FadeIn'
-import { formatDate } from '@/lib/formatDate'
-import useSWR from 'swr'
-import { MDXComponents } from '@/components/MDXComponents'
 import { fetchAxiosAPI } from '@/request/request'
 import { BaseData } from '@/types/global'
-import ReactMarkdown from 'react-markdown';
-import { PageLinks } from '@/components/PageLinks'
-import { Border } from '@/components/Border';
+import useSWR from 'swr'
 
 type Props = {
   params: any
 }
 
 export const ViewWork = ({ params: { id } }: Props) => {
-    const fetcher = (url: string) => {
-        console.log('Fetching API URL:', url) // Log the URL here
-        return fetchAxiosAPI(url)
-    }
+  const fetcher = (url: string) => {
+    console.log('Fetching API URL:', url) // Log the URL here
+    return fetchAxiosAPI(url)
+  }
 
-    const { data: workData } = useSWR<BaseData>(
-    `/works/${id}`,
-    fetcher,
-    )
-    const currentId = id;
-    const {data: allBlogs} = useSWR<BaseData>(
-        `/posts/`,
-        fetcher,
-    )
-    // let moreArticles = allBlogs?.data
-    // let moreArticles = Object.values(allBlogs); 
-    // .filter(({ id }) => id !== currentId)
-    // .slice(0, 2);
-  
-    // console.log('allblogs: ', allBlogs);
+  const { data: workData } = useSWR<BaseData>(`/works/${id}`, fetcher)
+  const currentId = id
+  const { data: allBlogs } = useSWR<BaseData>(`/posts/`, fetcher)
 
-    if (!workData || !workData.data) return null // Added check for blogData.data
-    const work = workData.data
-    const workContent = work.content; 
+  if (!workData || !workData.data) return null // Added check for blogData.data
+  const work = workData.data
+  const workContent = work.content
 
-    return (
-        <></>
-    
-  )
+  return <></>
 }
