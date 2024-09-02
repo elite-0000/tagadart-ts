1ad73546b8cf4e02c9626e905a8940255a97ea50
elite-0000 <elite.alphapoint1990@gmail.com>
2024-09-02 05:33:40 -0400

    code review in about&&contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
-import { useId } from 'react'
 import { type Metadata } from 'next'
 import Link from 'next/link'
+import { useId } from 'react'
 
 import { Border } from '@/components/Border'
 import { Button } from '@/components/Button'
