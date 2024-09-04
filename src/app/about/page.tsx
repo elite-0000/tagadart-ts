f1a57de9655920b48fa3dba3569e8c398856f6e6
elite-0000 <elite.alphapoint1990@gmail.com>
2024-09-04 04:57:38 -0400

    update about page - add readmore section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,6 +7,7 @@ import { PageIntro } from '@/components/sections/PageIntro'
 import Culture from '@/components/Culture'
 import Team from '@/components/Team'
 import { RestQueryParams } from '@/types/global'
+import { PageLinks } from '@/components/PageLinks'
 
 export const metadata: Metadata = {
   title: 'About Us',
@@ -23,8 +24,17 @@ export default async function About() {
     'teamSection.members',
     'teamSection.members.avatar',
   ]
+  const populateBlog = [
+    'pageIntro',
+    'blogSection',
+    'blogSection.posts',
+    'blogSection.posts.pageIntro',
+    'blogSection.posts.eyebrow',
+    'blogSection.posts.content',
+    'blogSection.posts.author',
+  ]
 
-  const defaultQueryParams: RestQueryParams = {
+  const aboutQueryParams: RestQueryParams = {
     populate: populateAbout,
     publicationState: 'preview',
     pagination: {
@@ -32,18 +42,36 @@ export default async function About() {
       pageSize: 10,
     },
   }
+
+  const blogQueryParams: RestQueryParams = {
+    populate: populateBlog,
+    publicationState: 'preview',
+    pagination: {
+      page: 1,
+      pageSize: 10,
+    },
+  }
   
-  let aboutData;
+  let aboutData, blogData;
   try {
-    aboutData = await fetchAxiosAPI('about-us-page', defaultQueryParams)
+    aboutData = await fetchAxiosAPI('about-us-page', aboutQueryParams)
   } catch (error) {
     // Handle the error appropriately here
     console.error('Failed to load about data:', error)
     return <div>Failed to load data</div>
   }
+
+  try {
+    blogData = await fetchAxiosAPI('blog-page', blogQueryParams)
+  } catch (error) {
+    // Handle the error appropriately here
+    console.error('Failed to load blog data:', error)
+    return <div>Failed to load data</div>
+  }
   
   const { pageIntro, cultureSection, teamSection } = aboutData?.data || {};
-  
+  const { blogSection } = blogData?.data
+  const {posts} = blogSection;
   return (
     <>
       {pageIntro && 
@@ -55,12 +83,12 @@ export default async function About() {
       <Culture culturecard={cultureSection} />
 
       <Team teamCard={teamSection} />
-      {/* <PageLinks
+      <PageLinks
         className="mt-24 sm:mt-32 lg:mt-40"
         title="From the blog"
         // intro="Our team of experienced designers and developers has just one thing on their mind; working on your ideas to draw a smile on the face of your users worldwide. From conducting Brand Sprints to UX Design."
-        pages={blogArticles}
-      /> */}
+        pages={posts}
+      />
 
       <ContactSection />
     </>
