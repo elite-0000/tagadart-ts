c73b343de81d71ad7cff105e70cfdedd5c928b8c
elite-0000 <elite.alphapoint1990@gmail.com>
2024-09-10 14:44:50 +0200

    remove default local prefix

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,7 +4,7 @@ import { createSharedPathnamesNavigation } from 'next-intl/navigation'
 export const routing = defineRouting({
   // A list of all locales that are supported
   locales: ['fr', 'en'],
-  //   localePrefix: 'as-needed',
+  localePrefix: 'as-needed',
   // Used when no locale matches
   defaultLocale: 'fr',
 })
