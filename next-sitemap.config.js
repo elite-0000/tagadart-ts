6792e79ec5e870757ef7d5b7e33358318fb2933f
elite-0000 <elite.alphapoint1990@gmail.com>
2024-10-29 10:49:39 +0100

    sitemap wip

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://example.com',
+  siteUrl: process.env.SITE_URL || 'https://tagadart.ch',
   generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
   // ...other options
 }
