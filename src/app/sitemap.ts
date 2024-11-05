07dc1f587918ebe27dff26c1d73d7f8e459bbb0e
elite-0000 <elite.alphapoint1990@gmail.com>
2024-11-05 13:48:21 -0500

    fix the sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,10 @@
-// app/sitemap.ts
 import { MetadataRoute } from 'next';
 import {
-    fetchClients,
-    fetchHomePage,
     fetchPosts,
     fetchProjects,
     fetchServices,
-  } from '@/request/fetch'
-  
+} from '@/request/fetch';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
 
@@ -26,6 +23,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/services`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly' as const,
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/projects`,
       lastModified: new Date(),
       changeFrequency: 'monthly' as const,
@@ -39,25 +42,37 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     },
   ];
 
-  // Fetch dynamic data from Strapi
-  const [posts] = await Promise.all([fetchPosts()]);
+  const [projects, posts, services] = await Promise.all([
+    fetchProjects(),
+    fetchPosts(),
+    fetchServices()
+  ]);
 
-  // Define dynamic routes for pages
-//   const pageRoutes = pages.map((page) => ({
-//     url: `${baseUrl}/${page.attributes.locale}/${page.attributes.slug}`,
-//     lastModified: new Date(page.attributes.updatedAt),
-//     changeFrequency: 'weekly' as const,
-//     priority: 0.7,
-//   }));
-
-  // Define dynamic routes for blog posts
   const postRoutes = posts.map((post: any) => ({
-    url: `${baseUrl}/posts/${post.attributes.slug}`,
-    lastModified: new Date(post.attributes.updatedAt),
+    url: `${baseUrl}/posts/${post.slug}`,
+    lastModified: new Date(post.updatedAt),
     changeFrequency: 'weekly' as const,
     priority: 0.6,
   }));
 
-  // Return combined sitemap routes
-  return [...staticRoutes, ...postRoutes];
+  const serviceRoutes = services.map((service: any) => ({
+    url: `${baseUrl}/services-page/${service.slug}`,
+    lastModified: new Date(service.updatedAt),
+    changeFrequency: 'monthly' as const,
+    priority: 0.7,
+  }));
+
+  const projectRoutes = projects.map((project: any) => ({
+    url: `${baseUrl}/projects/${project.slug}`,
+    lastModified: new Date(project.updatedAt),
+    changeFrequency: 'monthly' as const,
+    priority: 0.8,
+  }));
+
+  return [
+    ...staticRoutes,
+    ...projectRoutes,
+    ...serviceRoutes,
+    ...postRoutes
+  ];
 }
