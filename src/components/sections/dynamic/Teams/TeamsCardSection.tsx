9e66a890c389e758d0c84274ab0d80aaa4c6e7fe
elite-0000 <elite.alphapoint1990@gmail.com>
2024-11-12 17:08:29 +0100

    cultujre page

--- src/components/sections/dynamic/Teams/TeamsCardSection.tsx
+++ src/components/sections/dynamic/Teams/TeamsCardSection.tsx
@@ -36,7 +36,7 @@ const RenderContent: React.FC<RenderContentProps> = ({
           <SectionIntro {...sectionIntro} />
           <div className="space-y-24">
             <FadeIn>
-              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              <div>
                 <div className="lg:col-span-3">
                   <ul
                     role="list"
