80e173015b0a77e5199d463654c4cf5535da5032
elite-0000 <elite.alphapoint1990@gmail.com>
2024-11-12 09:22:07 -0500

    npm lint&&format

--- src/request/Fetcher.tsx
+++ src/request/Fetcher.tsx
@@ -1,32 +1,31 @@
-'use client';
+'use client'
 
-import React, { ReactNode, useEffect, useState } from "react";
-import useSWR, { KeyedMutator } from "swr";
-import { defaultQueryParams, fetchUseSWR } from "@/request/request";
-import { Data, RestQueryParams } from "@/types/global";
-import * as qs from "qs";
+import React, { ReactNode, useEffect, useState } from 'react'
+import useSWR, { KeyedMutator } from 'swr'
+import { defaultQueryParams, fetchUseSWR } from '@/request/request'
+import { Data, RestQueryParams } from '@/types/global'
+import * as qs from 'qs'
 
 interface FetcherChildProps<T> {
-  data: Data;
-  currentPage: number;
-  totalPages: number;
-  goToPage: (page: number) => void;
-  isLoading: boolean;
-  mutate: KeyedMutator<T>;
-  hasNextPage: boolean;
-  loadMore: () => void;
+  data: Data
+  currentPage: number
+  totalPages: number
+  goToPage: (page: number) => void
+  isLoading: boolean
+  mutate: KeyedMutator<T>
+  hasNextPage: boolean
+  loadMore: () => void
 }
 
 interface FetcherProps<T> {
-  url: string;
-  sort?: string;
-  populate?: string | object;
-  filters?: [];
-  paginationMode?: "infinite" | "pagination" | "off";
-  params?: RestQueryParams;
-  children: (fetchData: FetcherChildProps<T>) => ReactNode;
+  url: string
+  sort?: string
+  populate?: string | object
+  filters?: []
+  paginationMode?: 'infinite' | 'pagination' | 'off'
+  params?: RestQueryParams
+  children: (fetchData: FetcherChildProps<T>) => ReactNode
 }
-
 
 function Fetcher<T>({
   url,
@@ -37,63 +36,65 @@ function Fetcher<T>({
   sort,
   populate,
 }: FetcherProps<T>) {
-  
-  const [currentPage, setCurrentPage] = useState(1);
-  const [accumulatedData, setAccumulatedData] = useState<Data>();
+  const [currentPage, setCurrentPage] = useState(1)
+  const [accumulatedData, setAccumulatedData] = useState<Data>()
 
-  const effectiveFilters = { ...params.filters, ...filters };
-  const effectivePopulate = 'pageIntro.cover';
-  
-  const effectiveSort = sort || params.sort;
+  const effectiveFilters = { ...params.filters, ...filters }
+  const effectivePopulate = 'pageIntro.cover'
+
+  const effectiveSort = sort || params.sort
   const effectiveParams = {
     ...params,
     filters: effectiveFilters,
     sort: effectiveSort,
     populate: effectivePopulate,
-    pagination: paginationMode === "off" ? undefined : { ...params.pagination, page: currentPage },
-  };
+    pagination:
+      paginationMode === 'off'
+        ? undefined
+        : { ...params.pagination, page: currentPage },
+  }
 
   const queryString = qs.stringify(effectiveParams, {
     encode: false,
-    arrayFormat: "indices",
+    arrayFormat: 'indices',
     allowDots: false,
-  });
+  })
   const { data, error, mutate } = useSWR<any>(
     `${url}?${queryString}`,
     fetchUseSWR,
     {
       refreshInterval: 30000,
-    }
-  );
+    },
+  )
 
   const hasNextPage =
-    data?.meta?.pagination?.page < data?.meta?.pagination?.pageCount;
+    data?.meta?.pagination?.page < data?.meta?.pagination?.pageCount
 
   const goToPage = (pageNum: number) => {
-    setCurrentPage(pageNum);
-  };
+    setCurrentPage(pageNum)
+  }
 
   useEffect(() => {
     if (data) {
-      if (paginationMode === "infinite") {
+      if (paginationMode === 'infinite') {
         setAccumulatedData((prevData) => ({
           data: prevData ? [...prevData.data, ...data.data] : [...data.data],
           meta: data.meta,
-        }));
+        }))
       } else {
-        setAccumulatedData(data);
+        setAccumulatedData(data)
       }
     }
-  }, [accumulatedData, data, paginationMode]);
+  }, [accumulatedData, data, paginationMode])
 
   const loadMore = () => {
     if (hasNextPage) {
-      setCurrentPage((prev) => prev + 1);
+      setCurrentPage((prev) => prev + 1)
     }
-  };
+  }
 
-  if (!accumulatedData) return null; // Loading state can be handled differently
-  if (error) return <div>Error loading data...</div>;
+  if (!accumulatedData) return null // Loading state can be handled differently
+  if (error) return <div>Error loading data...</div>
 
   return children({
     data: accumulatedData,
@@ -104,7 +105,7 @@ function Fetcher<T>({
     totalPages: data?.meta?.pagination?.pageCount || 0,
     loadMore,
     hasNextPage,
-  });
+  })
 }
 
-export default Fetcher;
+export default Fetcher
