39734bf80fd5117398702e95e2495c7e94a70fab
elite-0000 <elite.alphapoint1990@gmail.com>
2024-11-12 17:08:29 +0100

    npm run lint&&format&&build

--- src/app/[locale]/projects/[id]/page.tsx
+++ src/app/[locale]/projects/[id]/page.tsx
@@ -18,13 +18,12 @@ type Props = {
   }
 }
 
-
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const project = await fetchProject(params.id)
   return generatePageMetadata({
     data: project,
     type: 'project',
-    id: params.id
+    id: params.id,
   })
 }
 
