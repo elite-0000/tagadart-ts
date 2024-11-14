7bdfe6315a259519034bbd1e549245ecae1eff08
elite-0000 <elite.alphapoint1990@gmail.com>
2024-11-14 18:51:55 -0500

    update the herosection

--- src/components/sections/dynamic/Hero/HeroSection.tsx
+++ src/components/sections/dynamic/Hero/HeroSection.tsx
@@ -154,6 +154,245 @@ const RenderContent: React.FC<HeroProps> = ({
           </div>
         </div>
       )
+    case 3: 
+      return (
+        <div className="relative bg-white">
+          <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
+            <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-48 lg:pt-40 xl:col-span-6">
+              <Container>
+                <FadeIn>
+                  <div className="mx-auto max-w-lg lg:mx-0">
+                    {heroSection?.logo && (
+                      <NextCloudinaryImage
+                        src={heroSection.logo.url}
+                        alt={heroSection.logo.alternativeText}
+                        width={124}
+                        height={48}
+                        className="my-8"
+                      />
+                    )}
+                    <h2 className="text-base font-semibold leading-7 text-primary-600">
+                          {heroSection?.sectionIntro.eyebrow}
+                    </h2>
+                    <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl lg:text-balance">
+                      {heroSection?.sectionIntro.title}
+                    </p>
+                    <div className="mt-6 text-lg leading-8 text-gray-600">
+                      <BasicMarkdown>
+                        {heroSection?.sectionIntro.content}
+                      </BasicMarkdown>
+                    </div>
+                    {heroSection?.buttons && heroSection.buttons.length > 0 && (
+                        <div className="mt-10 flex items-center gap-x-6">
+                          {heroSection.buttons.map((button) => (
+                            <Link
+                              key={button.id}
+                              href={button.link}
+                              className={`${
+                                button.type === 'primary'
+                                  ? 'rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600'
+                                  : 'text-sm font-semibold leading-6 text-gray-900'
+                              }`}
+                            >
+                              {button.text}
+                              {button.type === 'secondary' && (
+                                <span aria-hidden="true">→</span>
+                              )}
+                            </Link>
+                          ))}
+                        </div>
+                    )}
+                  </div>
+                </FadeIn>
+              </Container>
+            </div>
+          
+            <div className="flex justify-center items-center lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
+            {heroSection?.sectionIntro?.cover && (
+              <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+                <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+                  <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                    {(() => {
+                      const fileType = heroSection.sectionIntro.cover?.provider_metadata?.resource_type;
+                      const url = heroSection.sectionIntro.cover.url;
+
+                      if (fileType === 'image') {
+                        return (
+                          <Image
+                            src={url}
+                            alt={heroSection.sectionIntro.cover.alternativeText || 'Cover Image'}
+                            width={heroSection.sectionIntro.cover.width}
+                            height={heroSection.sectionIntro.cover.height}
+                            className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                          />
+                        );
+                      } else if (embedVideo !== undefined && embedVideo) {
+                        let embedUrl;
+                        if (embedVideo.includes('youtube.com/embed')) {
+                          embedUrl = embedVideo;
+                        }
+                        else if (embedVideo.includes('youtube.com') || embedVideo.includes('youtu.be')) {
+                          const videoId = embedVideo.includes('youtube.com')
+                            ? new URL(embedVideo).searchParams.get('v')
+                            : embedVideo.split('/').pop();
+                          embedUrl = `https://www.youtube.com/embed/${videoId}`;
+                        }
+
+                        return (
+                          <iframe
+                            width="700"
+                            height="600"
+                            src={embedUrl}
+                            title={heroSection.sectionIntro.cover.alternativeText || 'YouTube Video'}
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                            className="w-full rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                          ></iframe>
+                        );
+                      } else if (fileType === 'video') {
+                          return (
+                            <video width="700" height="600" controls preload="none" >
+                              <source src={url} type="video/mp4" />
+                              Your browser does not support the video tag.
+                            </video>
+                          );
+                      }
+                      return null;
+                    })()}
+                  </div>
+                </div>
+              </div>
+            )}
+            </div>
+          </div>
+        </div>
+        
+      )
+    case 5: 
+      return (
+        <div className="relative isolate pt-14">
+          <div
+            aria-hidden="true"
+            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+          <div className="justify-center items-center py-24 sm:py-32 lg:pb-40">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl text-center">
+                <h2 className="text-base font-semibold leading-7 text-primary-600">
+                        {heroSection?.sectionIntro.eyebrow}
+                </h2>
+                <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl lg:text-balance">
+                  {heroSection?.sectionIntro.title}
+                </p>
+                <div className="mt-6 text-lg leading-8 text-gray-600">
+                  <BasicMarkdown>
+                    {heroSection?.sectionIntro.content}
+                  </BasicMarkdown>
+                </div>
+                <div className="mt-10 flex justify-center items-center gap-x-6">
+                  {heroSection?.buttons && heroSection.buttons.length > 0 && (
+                    <div className="mt-10 flex items-center gap-x-6">
+                      {heroSection.buttons.map((button) => (
+                        <Link
+                          key={button.id}
+                          href={button.link}
+                          className={`${
+                            button.type === 'primary'
+                              ? 'rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600'
+                              : 'text-sm font-semibold leading-6 text-gray-900'
+                          }`}
+                        >
+                          {button.text}
+                          {button.type === 'secondary' && (
+                            <span aria-hidden="true">→</span>
+                          )}
+                        </Link>
+                      ))}
+                    </div>
+                  )}
+                </div>
+                
+              </div>
+              
+                {heroSection?.sectionIntro?.cover && (
+                  <div className="mx-auto mt-16 flex max-w-2xl justify-center sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+                    <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+                      <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                        {(() => {
+                          const fileType = heroSection.sectionIntro.cover?.provider_metadata?.resource_type;
+                          const url = heroSection.sectionIntro.cover.url;
+
+                          if (fileType === 'image') {
+                            return (
+                              <Image
+                                src={url}
+                                alt={heroSection.sectionIntro.cover.alternativeText || 'Cover Image'}
+                                width={heroSection.sectionIntro.cover.width}
+                                height={heroSection.sectionIntro.cover.height}
+                                className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                              />
+                            );
+                          } else if (embedVideo !== undefined && embedVideo) {
+                            let embedUrl;
+                            if (embedVideo.includes('youtube.com/embed')) {
+                              embedUrl = embedVideo;
+                            }
+                            else if (embedVideo.includes('youtube.com') || embedVideo.includes('youtu.be')) {
+                              const videoId = embedVideo.includes('youtube.com')
+                                ? new URL(embedVideo).searchParams.get('v')
+                                : embedVideo.split('/').pop();
+                              embedUrl = `https://www.youtube.com/embed/${videoId}`;
+                            }
+
+                            return (
+                              <iframe
+                                width="700"
+                                height="600"
+                                src={embedUrl}
+                                title={heroSection.sectionIntro.cover.alternativeText || 'YouTube Video'}
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                                className="w-full rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                              ></iframe>
+                            );
+                          } else if (fileType === 'video') {
+                              return (
+                                <video width="700" height="600" controls preload="none" >
+                                  <source src={url} type="video/mp4" />
+                                  Your browser does not support the video tag.
+                                </video>
+                              );
+                          }
+                          return null;
+                        })()}
+                      </div>
+                    </div>
+                  </div>
+                )}
+            </div>
+          </div>
+          <div
+            aria-hidden="true"
+            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          >
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+        </div>
+      )
     default:
       return (
         <div className="relative isolate overflow-hidden bg-white">
