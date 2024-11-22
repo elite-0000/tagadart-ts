aac828b00f9bfef7bed71b859fd0f1534327a31c
elite-0000 <elite.alphapoint1990@gmail.com>
2024-11-22 17:57:20 +0100

    removbe api url

--- src/app/[locale]/[[...slug]]/page.tsx
+++ src/app/[locale]/[[...slug]]/page.tsx
@@ -33,7 +33,6 @@ export default async function PageRoute({ params }: Props) {
   const contentSections = page?.data[0]?.structure
   return (
     <Container>
-      <h1>api url{process.env.NEXT_PUBLIC_API_URL}</h1>
       {contentSections?.map((section: Section & PageIntro) =>
         componentResolver({ section, designType: section.designType }),
       )}
