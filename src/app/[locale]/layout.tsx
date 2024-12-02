302d5075798a0f91b060bb6e2d54ea599fedb13a
elite-0000 <elite.alphapoint1990@gmail.com>
2024-12-02 01:47:14 -0500

    remove log

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -40,7 +40,6 @@ export default async function LocaleLayout({
   params: { locale: string }
 }) {
   const messages = await getMessages()
-  console.log("fonts: ", monaSans);
   return (
     <html
       lang={locale}
