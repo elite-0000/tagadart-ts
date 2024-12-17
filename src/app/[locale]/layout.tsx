267b7a402b1633fa7f09e419b497d05052216381
elite-0000 <elite.alphapoint1990@gmail.com>
2024-12-17 11:10:52 -0500

    test for optimize font

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,31 +6,13 @@ import '@/styles/tailwind.css'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
 import { RootLayout } from '@/components/ui/RootLayout'
-import Script from 'next/script'
-import { Inter } from 'next/font/google'
-import localFont from 'next/font/local'
 
 export const metadata: Metadata = {
   title: {
     template: '%s - Tagadart',
     default: 'Tagadart - Agence Digitale Eco Responsable Lausanne',
   },
-  icons: {
-    icon: '/favicon.ico',
-  },
 }
-
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
-})
-
-const monaSans = localFont({
-  src: '../../../public/fonts/Mona-Sans.var.woff2',
-  variable: '--font-mona-sans',
-  display: 'swap',
-})
 
 export default async function LocaleLayout({
   children,
@@ -39,28 +21,30 @@ export default async function LocaleLayout({
   children: React.ReactNode
   params: { locale: string }
 }) {
+  // Providing all messages to the client
+  // side is the easiest way to get started
   const messages = await getMessages()
 
   return (
-    <html
-      lang={locale}
-      className={`h-full bg-neutral-950 text-base antialiased ${inter.variable} ${monaSans.variable}`}
-    >
+    <html lang={locale} className="h-full bg-neutral-950 text-base antialiased">
       <head>
-        <Script
+        <script
+          async
           src="https://www.googletagmanager.com/gtag/js?id=AW-16700186470"
-          strategy="afterInteractive"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16700186470');
+            `,
+          }}
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-16700186470');
-        `}
-        </Script>
       </head>
       <body className="flex min-h-full flex-col">
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
         <NextIntlClientProvider messages={messages}>
           <RootLayout>{children}</RootLayout>
           <Toaster />
