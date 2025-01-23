e39eca950e0dcf738f5b1d3cbcea4b4df385a18d
elite-0000 <elite.alphapoint1990@gmail.com>
2025-01-23 17:34:00 -0500

    recover component

--- src/lib/componentResolver.tsx
+++ src/lib/componentResolver.tsx
@@ -23,124 +23,124 @@ export const componentResolver = ({
 }) => {
   console.log('component: ', section.__component)
   switch (section.__component) {
-    // case 'section.text-section':
-    //   return (
-    //     <Section className="m-auto max-w-5xl">
-    //       {section.title && (
-    //         <h2 className="text-2xl font-semibold">{section.title}</h2>
-    //       )}
-    //       <BasicMarkdown content={section.content} />
-    //     </Section>
-    //   )
-    // case 'section.blog-section':
-    //   return (
-    //     <PostsSection
-    //       key={section.id}
-    //       postsSection={section}
-    //       designType={designType}
-    //     />
-    //   )
-    // case 'section.projects-section':
-    //   return (
-    //     <ProjectsSection
-    //       key={section.id}
-    //       projectsSection={section}
-    //       designType={designType}
-    //     />
-    //   )
+    case 'section.text-section':
+      return (
+        <Section className="m-auto max-w-5xl">
+          {section.title && (
+            <h2 className="text-2xl font-semibold">{section.title}</h2>
+          )}
+          <BasicMarkdown content={section.content} />
+        </Section>
+      )
+    case 'section.blog-section':
+      return (
+        <PostsSection
+          key={section.id}
+          postsSection={section}
+          designType={designType}
+        />
+      )
+    case 'section.projects-section':
+      return (
+        <ProjectsSection
+          key={section.id}
+          projectsSection={section}
+          designType={designType}
+        />
+      )
 
-    // case 'section.services-section':
-    //   return (
-    //     <ServicesSection
-    //       key={section.id}
-    //       servicesSection={section}
-    //       designType={designType}
-    //     />
-    //   )
-    // case 'section.reference-section':
-    //   return (
-    //     <ReferenceSection
-    //       key={section.id}
-    //       referenceSection={section}
-    //       designType={designType}
-    //     />
-    //   )
-    // case 'section.cta':
-    //   return (
-    //     <CTASection
-    //       key={section.id}
-    //       ctaSection={section}
-    //       designType={designType}
-    //     />
-    //   )
-    // case 'section.pricing-section':
-    //   return (
-    //     <PricingSection
-    //       key={section.id}
-    //       pricingSection={section}
-    //       designType={designType}
-    //     />
-    //   )
-    // case 'section.team-section':
-    //   return (
-    //     <TeamsSection
-    //       key={section.id}
-    //       teamsSection={section}
-    //       designType={designType}
-    //     />
-    //   )
-    // case 'section.culture-section':
-    //   return (
-    //     <CultureSection
-    //       key={section.id}
-    //       culturesSection={section}
-    //       designType={designType}
-    //     />
-    //   )
-    // case 'section.contact-section':
-    //   return (
-    //     <ContactSection
-    //       key={section.id}
-    //       contactSection={section}
-    //       designType={designType}
-    //     />
-    //   )
-    // case 'section.page-intro':
-    //   return (
-    //     <PageIntroSection
-    //       key={section.id}
-    //       pageIntroSection={section}
-    //       designType={designType}
-    //     />
-    //   )
+    case 'section.services-section':
+      return (
+        <ServicesSection
+          key={section.id}
+          servicesSection={section}
+          designType={designType}
+        />
+      )
+    case 'section.reference-section':
+      return (
+        <ReferenceSection
+          key={section.id}
+          referenceSection={section}
+          designType={designType}
+        />
+      )
+    case 'section.cta':
+      return (
+        <CTASection
+          key={section.id}
+          ctaSection={section}
+          designType={designType}
+        />
+      )
+    case 'section.pricing-section':
+      return (
+        <PricingSection
+          key={section.id}
+          pricingSection={section}
+          designType={designType}
+        />
+      )
+    case 'section.team-section':
+      return (
+        <TeamsSection
+          key={section.id}
+          teamsSection={section}
+          designType={designType}
+        />
+      )
+    case 'section.culture-section':
+      return (
+        <CultureSection
+          key={section.id}
+          culturesSection={section}
+          designType={designType}
+        />
+      )
+    case 'section.contact-section':
+      return (
+        <ContactSection
+          key={section.id}
+          contactSection={section}
+          designType={designType}
+        />
+      )
+    case 'section.page-intro':
+      return (
+        <PageIntroSection
+          key={section.id}
+          pageIntroSection={section}
+          designType={designType}
+        />
+      )
 
-    // case 'section.features-section':
-    //   return (
-    //     <FeaturesSection
-    //       key={section.id}
-    //       featuresSection={section}
-    //       designType={designType}
-    //     />
-    //   )
-    // case 'section.hero-section':
-    //   return (
-    //     <>
-    //       <HeroSection
-    //         key={section.id}
-    //         heroSection={section}
-    //         designType={designType}
-    //       />
-    //       {/* <HeroSection key={section.id} heroSection={section} designType={1} />
-    //       <HeroSection key={section.id} heroSection={section} designType={2} />
-    //       <HeroSection key={section.id} heroSection={section} designType={3} />
-    //       <HeroSection key={section.id} heroSection={section} designType={4} /> */}
-    //     </>
-    //   )
+    case 'section.features-section':
+      return (
+        <FeaturesSection
+          key={section.id}
+          featuresSection={section}
+          designType={designType}
+        />
+      )
+    case 'section.hero-section':
+      return (
+        <>
+          <HeroSection
+            key={section.id}
+            heroSection={section}
+            designType={designType}
+          />
+          {/* <HeroSection key={section.id} heroSection={section} designType={1} />
+          <HeroSection key={section.id} heroSection={section} designType={2} />
+          <HeroSection key={section.id} heroSection={section} designType={3} />
+          <HeroSection key={section.id} heroSection={section} designType={4} /> */}
+        </>
+      )
 
-    // case 'section.testimonials':
-    //   return (
-    //     <TestimonialSection testimonialSection={section} key={section.id} />
-    //   )
+    case 'section.testimonials':
+      return (
+        <TestimonialSection testimonialSection={section} key={section.id} />
+      )
 
     default:
       return null
